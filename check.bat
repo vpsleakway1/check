ecbc23ad39354a86a70cb704e15c00d9a95ef17f
vpsleakway1 <122168313+vpsleakway1@users.noreply.github.com>
2023-12-06 23:56:51 +0700

    Create check.bat

---
+++ check.bat
@@ -0,0 +1,2 @@
+@echo off
+echo tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
